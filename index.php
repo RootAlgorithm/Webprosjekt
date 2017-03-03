982eec96dc8f43890ee13be498219d243ff6a36f
RootAlgorithm <morkai16@student.westerdals.no>
2017-03-03 19:24:25 +0100

    Oppdaterte litt på index.php TG-11

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 		<title>PHP Test</title>
 	</head>
 	<body>
-	<?php echo '<p>Om du ser dette så funker PHP greit på denne serveren</p>'; ?>
+	<?php echo '<h1>Foreløpig testside</h1>'; ?>
+	<?php echo '<p>Om du ser dette så funker PHP greit på denne serveren</p><br>'; ?>
+	<?php echo '<p>Jeg jobber kontinuerlig med å få implementert et enkelt Content Management System (CMS)</p><br>'; ?>
 	</body>
 </html>
