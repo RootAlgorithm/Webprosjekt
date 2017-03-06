d7a0ef9eb1d04e469194e2ade00791f8c927540c
RootAlgorithm <morkai16@student.westerdals.no>
2017-03-06 21:55:47 +0100

    La til header og footer

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<html>
+
+<head>
+
+<title>PRO101 Webprosjekt</title>
+
+</head>
+
+<body>
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<html>
-	<head>
-		<title>PHP Test</title>
-	</head>
-	<body>
+<?php
+require 'includes/header.php';
+?>
 	<?php echo '<h1>Foreløpig testside</h1>'; ?>
 	<?php echo '<p>Om du ser dette så funker PHP greit på denne serveren</p><br>'; ?>
 	<?php echo '<p>Jeg jobber kontinuerlig med å få implementert et enkelt Content Management System (CMS)</p><br>'; ?>
-	</body>
-</html>
+<?php
+require 'includes/footer.php';
+?>
