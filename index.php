e506bb67bfd32384e25d973659129880a2c6d1f0
Ritter <ritand16@student.westerdals.no>
2017-03-16 11:47:27 +0100

    La til en snutt med php for å øve meg c:

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
 	<?php echo '<p>Om du ser dette så funker PHP greit på denne serveren</p><br>'; ?>
 	<?php echo '<p>Jeg jobber kontinuerlig med å få implementert et enkelt Content Management System (CMS)</p><br>'; ?>
     <?php echo '<p>Prøver på nytt</p><br>'; ?>
+        
+    <?php 
+        $andreas = 'kul';
+        $kul = "Ja!";
+        echo $$andreas;
+    ?>
         <div>
             <p>Nå har Andreas også vært innom.</p>
         </div>
