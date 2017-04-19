9a044a9cb0934f1941510aabb759ee7be6a9290e
Ritter <ritand16@student.westerdals.no>
2017-04-19 13:09:21 +0200

    lager en mappe andreas legger ut det han holder på med

---
+++ prototype/andreas_proto/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        
+        <title>Andreas sin prototype</title>
+
+    </head>
+    <body>
+
+        <div>
+        
+        </div>
+
+    </body>
+</html>
