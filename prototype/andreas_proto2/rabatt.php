5086ebf0d73bebbb404ca4347ad78ff818f62856
Ritter <ritand16@student.westerdals.no>
2017-04-26 18:04:13 +0200

    laget en helt ny side og rammeverk, finn ut hva resten synes så langt, jeg er lei nå, sorry not sorry

---
+++ prototype/andreas_proto2/rabatt.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+    <title>Andreas sin prototype v2</title>
+
+</head>
+<body>
+
+<div id="container">
+
+    <div class="header_body">
+        <div class="header_icon"><a href="index.php"><img src="bilder/logo_v2.png"></a></div>
+        <div class="header_btn">
+            <div class="header_btn_ting"><a href="minside.php"><input type="header" value="Min Side"></a></div>
+            <div class="header_btn_ting"><a href="nerheten.php"><input type="header" value="I nærheten"></a></div>
+            <div class="header_btn_ting"><a href="rabatt.php"><input type="header" value="Rabatter"></a></div>
+            <div class="header_btn_ting"><a href="campus.php"><input type="header" value="På Campus"></a></div>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
