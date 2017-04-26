9bdacc419cfade4c3864aa4b5e9b35ed54982a59
RootAlgorithm <morkai16@student.westerdals.no>
2017-04-26 11:31:44 +0200

    deleted crap

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
-<?php
-
-
-
-?>
-
+<!DOCTYPE html>
 <html>
-	<head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" type="text/css" href="">
 
-        <link rel="stylesheet" type="text/css" href="stylesheet.css">
-		<title>PHP Test</title>
-	</head>
-	<body>
-	<h1>Foreløpig testside</h1></br>
-	<p>Om du ser dette så funker PHP greit på denne serveren</p><br>
-	<p></p><br>
-    	<p></p><br>
-        
-    
-        
-       
-        <div>
-            <p>Nå har Andreas også vært innom.</p>
-        </div>
-	</body>
+    <title>PROTOTYPE</title>
+
+</head>
+<body>
+
+<!-- Slideshow -->
+<div class="slideshowContainer">
+
+</div>
+
+<!-- Main window -->
+<div id="container">
+
+</div>
+</body>
 </html>
