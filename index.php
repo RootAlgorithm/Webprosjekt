8443f83d6c05da61b4e622c2d29876ccfac5e917
Ritter <ritand16@student.westerdals.no>
2017-05-03 20:40:29 +0200

    små endringer i config på main stort sett

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <?php
-    require "vendor/autoload.php";
-    require "config.php";
+require "vendor/autoload.php";
+require "config.php";
 
 
-    $slides = Slide::all();
-    ?>
+$slides = Slide::all();
+?>
 
 <?php
 require "includes/header.php";
 ?>
 
-<!-- Main window -->
+    <!-- Main window -->
 
     <div id="container">
 
-        <div class="header_body">
-            <div class="header_icon"><a href="index.php"><img src="Bilder/logo_v2.png"></a></div>
-            <div class="header_btn">
-                <div class="header_btn_ting"><a href="minside.php"><input type="header" value="Min Side"></a></div>
-                <div class="header_btn_ting"><a href="nerheten.php"><input type="header" value="I nærheten"></a></div>
-                <div class="header_btn_ting"><a href="rabatt.php"><input type="header" value="Rabatter"></a></div>
-                <div class="header_btn_ting"><a href="campus.php"><input type="header" value="På Campus"></a></div>
-            </div>
+    <div class="header_body">
+        <div class="header_icon"><a href="index.php"><img src="Bilder/logo_v2.png"></a></div>
+        <div class="header_btn">
+            <div class="header_btn_ting"><a href="minside.php"><input type="header" value="Min Side"></a></div>
+            <div class="header_btn_ting"><a href="nerheten.php"><input type="header" value="I nærheten"></a></div>
+            <div class="header_btn_ting"><a href="rabatt.php"><input type="header" value="Rabatter"></a></div>
+            <div class="header_btn_ting"><a href="campus.php"><input type="header" value="På Campus"></a></div>
         </div>
+    </div>
 
-        <div id="index_main_body">
-            <?php
+    <div id="index_main_body">
+        <?php
 
-            if (!$slides->isEmpty())
-            {
+        if (!$slides->isEmpty())
+        {
 
             foreach ($slides as $slide)
             {
@@ -41,45 +41,45 @@ require "includes/header.php";
             <a class="nextSlide" onclick="slidesFunction(1)">&#10095;</a>
             <script src="includes/slideshow.js"></script>
 
-            <?php } ?>
+        <?php } ?>
 
 
-            <div id="index_main_body_lesmer">
-                <div class="index_main_body_lesmercard">
-                    <img src="Bilder/rabattsymbol-crop-u3203.png">
-                    <h3>Rabatter</h3>
-                    <p>Her kan du se flotte rabatter <br/>
-                        basert på dine interesser. Vi <br/>
-                        oppdaterer hele tiden med <br/>
-                        nye rabatter</p>
-                    <input type="lesmercard" value="Les mer her!">
-                </div>
-                <div class="index_main_body_lesmercard">
-                    <img src="Bilder/skolesymbol-crop-u3212.png">
-                    <h3>Rabatter</h3>
-                    <p>Se det som skjer på Camppus som er <br/>
-                        interessant for akkurat deg, eller se gjennom <br/>
-                        etter andre spennende ting som skjer!</p><br/>
-                    <input type="lesmercard" value="Les mer her!">
-                </div>
-                <div class="index_main_body_lesmercard">
-                    <img src="Bilder/geotag.png">
-                    <h3>Rabatter</h3>
-                    <p>Se hva som er og skjer i nærheten, <br/>
-                        du finner alltid noe som er <br/>
-                        interessant for akkurat deg</p><br/>
-                    <input type="lesmercard" value="Les mer her!">
-                </div>
-                <div class="index_main_body_lesmercard">
-                    <img src="Bilder/minsidesymbol-crop-u3206.png">
-                    <h3>Rabatter</h3>
-                    <p>Legg til dine interesser og <br/>
-                        få innhold basert på dette</p><br/><br/>
-                    <input type="lesmercard" value="Les mer her!">
-                </div>
-
+        <div id="index_main_body_lesmer">
+            <div class="index_main_body_lesmercard">
+                <img src="Bilder/rabattsymbol-crop-u3203.png">
+                <h3>Rabatter</h3>
+                <p>Her kan du se flotte rabatter <br/>
+                    basert på dine interesser. Vi <br/>
+                    oppdaterer hele tiden med <br/>
+                    nye rabatter</p>
+                <input type="lesmercard" value="Les mer her!">
             </div>
+            <div class="index_main_body_lesmercard">
+                <img src="Bilder/skolesymbol-crop-u3212.png">
+                <h3>Rabatter</h3>
+                <p>Se det som skjer på Camppus som er <br/>
+                    interessant for akkurat deg, eller se gjennom <br/>
+                    etter andre spennende ting som skjer!</p><br/>
+                <input type="lesmercard" value="Les mer her!">
+            </div>
+            <div class="index_main_body_lesmercard">
+                <img src="Bilder/geotag.png">
+                <h3>Rabatter</h3>
+                <p>Se hva som er og skjer i nærheten, <br/>
+                    du finner alltid noe som er <br/>
+                    interessant for akkurat deg</p><br/>
+                <input type="lesmercard" value="Les mer her!">
+            </div>
+            <div class="index_main_body_lesmercard">
+                <img src="Bilder/minsidesymbol-crop-u3206.png">
+                <h3>Rabatter</h3>
+                <p>Legg til dine interesser og <br/>
+                    få innhold basert på dette</p><br/><br/>
+                <input type="lesmercard" value="Les mer her!">
+            </div>
+
         </div>
+    </div>
 
 <?php
 require "includes/footer.php";
