73a38f6cbe0d3ea70e64921cb6e438c92b08b020
Ritter <ritand16@student.westerdals.no>
2017-05-04 13:55:09 +0200

    endret på reg skjemaet stort sett

--- minside_reg.php
+++ minside_reg.php
@@ -14,27 +14,29 @@ require "includes/header.php";
     require "includes/header_link.php";
     ?>
 
-
+    <img class="minside_bilde" src="Bilder/Studenter.jpg">
     <div class="minside_schema">
-
+        <label for="fname">First Name</label>
         <input type="text" id="fname" name="brukernavn" placeholder="Brukernavn...">
 
-
+        <label for="fname">First Name</label>
         <input type="text" id="epost" name="epost" placeholder="Epost...">
 
-
+        <label for="fname">First Name</label>
         <input type="text" id="fname" name="fornavn" placeholder="Fornanvn...">
 
-
+        <label for="fname">First Name</label>
         <input type="text" id="ename" name="etternavn" placeholder="Etternavn...">
 
-
+        <label for="fname">First Name</label>
         <input type="password" id="passord" name="passord" placeholder="Passord...">
 
-
+        <label for="fname">First Name</label>
         <input type="password" id="password2" name="gjenntapassord" placeholder="Gjennta passord...">
+
+        <input type="submit" value="Submit">
     </div>
-    <img class="minside_bilde" src="Bilder/Studenter.jpg">
+
 
 </div>
 
