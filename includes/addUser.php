5442a20b1f117ac1aca67f1da23f528fea9427bb
RootAlgorithm <morkai16@student.westerdals.no>
2017-05-04 14:29:14 +0200

    updated, and fixed a bug

--- includes/addUser.php
+++ includes/addUser.php
@@ -5,3 +5,21 @@
  * Date: 04/05/17
  * Time: 13.38
  */
+
+require '../vendor/autoload.php';
+require '../config.php';
+
+$user = new User();
+$user->setAttribute('username', $_POST['']);
+$user->setAttribute('first-name', $_POST['']);
+$user->setAttribute('last-name', $_POST['']);
+$user->setAttribute('email', $_POST['']);
+
+if($_POST[''] == $_POST[''])
+{
+    $user->setAttribute('password', password_hash($_POST[''], PASSWORD_BCRYPT));
+}
+$user->save();
+
+header('Location: http://tek.westerdals.no/~ritand16/Webprosjekt');
+die();
