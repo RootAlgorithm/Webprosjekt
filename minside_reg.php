2eca49a9158a8ba485645d4ed8d6455361d734a8
Ritter <ritand16@student.westerdals.no>
2017-05-04 13:42:06 +0200

    lagde styles1 for implementering, og endret litt i html filene

--- minside_reg.php
+++ minside_reg.php
@@ -14,6 +14,26 @@ require "includes/header.php";
     require "includes/header_link.php";
     ?>
 
+
+    <div class="minside_schema">
+
+        <input type="text" id="fname" name="brukernavn" placeholder="Brukernavn...">
+
+
+        <input type="text" id="epost" name="epost" placeholder="Epost...">
+
+
+        <input type="text" id="fname" name="fornavn" placeholder="Fornanvn...">
+
+
+        <input type="text" id="ename" name="etternavn" placeholder="Etternavn...">
+
+
+        <input type="password" id="passord" name="passord" placeholder="Passord...">
+
+
+        <input type="password" id="password2" name="gjenntapassord" placeholder="Gjennta passord...">
+    </div>
     <img class="minside_bilde" src="Bilder/Studenter.jpg">
 
 </div>
