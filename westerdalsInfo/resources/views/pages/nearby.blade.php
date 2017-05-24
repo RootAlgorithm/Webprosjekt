f142921f3d50ab07e937e84d8a3c2c28e2aaed0d
andreasholteritter <andreasholteritter@gmail.com>
2017-05-24 13:03:09 +0200

    endret nearby.blade.php

--- westerdalsInfo/resources/views/pages/nearby.blade.php
+++ westerdalsInfo/resources/views/pages/nearby.blade.php
@@ -7,10 +7,14 @@
     <div class="container">
         <div class="row">
             @foreach($posts as $post)
-
-                <div class="col-md-4">
+                <div class="col-md-6">
+                    <img src="#">
+                </div>
+                <div class="col-md-6">
                     <h2>{{ $post->title }}</h2>
                     <p>{{ $post->body }}</p>
+                    <p><b>#Dato: #</b>#</p>
+                    <p><b>#Sted: #</b>#</p>
                 </div>
 
             @endforeach
