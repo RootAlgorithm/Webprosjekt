08dcc7cfb9823bf35333f60877c5e80bd93caf9e
endremikal <endremikal@hotmail.com>
2017-05-25 14:56:33 +0200

    byttet plass på på campus og i nærheten på forsiden

--- westerdalsInfo/resources/views/includes/main-links.blade.php
+++ westerdalsInfo/resources/views/includes/main-links.blade.php
@@ -9,19 +9,19 @@
 
 <div class="col-md-3">
     <div>
-        {{ Html::image('img/campus.png', '', array('class' => 'center-block img-responsive', 'style' => 'width: 50%')) }}
-        <h4 class="text-center">På campus</h4>
-        <p class="text-center">Se det som skjer på Campus som er interessant for akkurat deg, eller se gjennom etter andre spennende ting som skjer</p>
-        <a class="btn btn-info btn-block main-links-btn" href="{{ url('/campus') }}">Les mer her</a>
+        {{ Html::image('img/geotag.png', '', array('class' => 'center-block img-responsive', 'style' => 'width: 50%')) }}
+        <h4 class="text-center">I nærheten</h4>
+        <p class="text-center">Se hva som er og skjer i nærheten, du finner alltid noe som er er interessant for akkurat deg.</p>
+        <a class="btn btn-info btn-block main-links-btn" href="{{ url('/nearby') }}">Les mer her</a>
     </div>
 </div>
 
 <div class="col-md-3">
     <div>
-        {{ Html::image('img/geotag.png', '', array('class' => 'center-block img-responsive', 'style' => 'width: 50%')) }}
-        <h4 class="text-center">I nærheten</h4>
-        <p class="text-center">Se hva som er og skjer i nærheten, du finner alltid noe som er er interessant for akkurat deg.</p>
-        <a class="btn btn-info btn-block main-links-btn" href="{{ url('/nearby') }}">Les mer her</a>
+        {{ Html::image('img/campus.png', '', array('class' => 'center-block img-responsive', 'style' => 'width: 50%')) }}
+        <h4 class="text-center">På campus</h4>
+        <p class="text-center">Se det som skjer på Campus som er interessant for akkurat deg, eller se gjennom etter andre spennende ting som skjer</p>
+        <a class="btn btn-info btn-block main-links-btn" href="{{ url('/campus') }}">Les mer her</a>
     </div>
 </div>
 
