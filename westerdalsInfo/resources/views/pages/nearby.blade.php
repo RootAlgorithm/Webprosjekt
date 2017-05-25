00f70dcfdbcdd62f459294b13444c23520a7d6df
andreasholteritter <andreasholteritter@gmail.com>
2017-05-25 16:15:28 +0200

    endret på undersidene litt, begynte å forme ut nearby

--- westerdalsInfo/resources/views/pages/nearby.blade.php
+++ westerdalsInfo/resources/views/pages/nearby.blade.php
@@ -4,17 +4,30 @@
 
 @section('content')
 
+    <div class="extended_header">
+        <div class="col-md-6">
+            <img src="#">
+        </div>
+        <div class="col-md-6">
+            <h1>Header</h1>
+            <p>Leter du etter en ekstrajobb mens du studerer eller vil du bare vite
+                om det noe som skjer i studentbaren?
+                </br>
+                Her vil du (om du har registrert en bruker) få anbefalte
+                arrangementer, Stillinger eller foreninger spesielt rettet mot deg!
+                </br>
+                Skjer det noe på Campus er dette plassen du får vite det!</p>
+        </div>
+    </div>
+
     <div class="container">
+
         <div class="row">
             @foreach($posts as $post)
-                <div class="col-md-6">
-                    <img src="#">
-                </div>
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <h2>{{ $post->title }}</h2>
                     <span>{!! $post->body !!}</span>
                     <p><b>Oppdatert: </b>{{ date( 'j M Y - H:i', strtotime($post->updated_at)) }}</p>
-                    <p><b>Sted: </b>#</p>
                 </div>
 
             @endforeach
