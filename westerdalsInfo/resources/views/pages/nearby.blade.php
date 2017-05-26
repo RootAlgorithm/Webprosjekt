b85e775d7849e9c1d5dc4d4c9f9991a7d4a6973b
andreasholteritter <andreasholteritter@gmail.com>
2017-05-26 15:00:33 +0200

    endret nearby og campus så den får en underheader hver

--- westerdalsInfo/resources/views/pages/nearby.blade.php
+++ westerdalsInfo/resources/views/pages/nearby.blade.php
@@ -4,19 +4,21 @@
 
 @section('content')
 
-    <div class="extended_header">
-        <div class="col-md-6">
-            <img src="#">
-        </div>
-        <div class="col-md-6">
-            <h1>Header</h1>
-            <p>Leter du etter en ekstrajobb mens du studerer eller vil du bare vite
-                om det noe som skjer i studentbaren?
-                </br>
-                Her vil du (om du har registrert en bruker) få anbefalte
-                arrangementer, Stillinger eller foreninger spesielt rettet mot deg!
-                </br>
-                Skjer det noe på Campus er dette plassen du får vite det!</p>
+    <div class="row" style="background-color: #343434">
+        <div class="container">
+            <div class="col-md-6">
+                {{ Html::image('img/NearbyHeader.jpg', '', array('class' => 'img-responsive subpage-image-header')) }}
+            </div>
+            <div class="col-md-6">
+                <h1 class="white-text">Header</h1>
+                <p class="white-text">Leter du etter en ekstrajobb mens du studerer eller vil du bare vite
+                    om det noe som skjer i studentbaren?
+                    </br>
+                    Her vil du (om du har registrert en bruker) få anbefalte
+                    arrangementer, Stillinger eller foreninger spesielt rettet mot deg!
+                    </br>
+                    Skjer det noe på Campus er dette plassen du får vite det!</p>
+            </div>
         </div>
     </div>
 
