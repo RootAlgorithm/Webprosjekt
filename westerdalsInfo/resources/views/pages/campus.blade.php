60eb4d029cd9c09dd1a1ca3e5759be322845956d
andreasholteritter <andreasholteritter@gmail.com>
2017-05-26 12:20:28 +0200

    endret på scss og på blades.current

--- westerdalsInfo/resources/views/pages/campus.blade.php
+++ westerdalsInfo/resources/views/pages/campus.blade.php
@@ -4,6 +4,26 @@
 
 @section('content')
 
+    <div class="helt_aktuelt">
+        <div class="helt_aktuelt_head">
+            <h1>Aktuelt</h1>
+        </div>
+        <div class="helt_aktuelt_body">
+            @for($i = 0; $i < 3; $i++)
+                <div class="div-md-4">
+                    <div class="hoved-kort">
+                        <img class="hoved-bilde" src="#">
+                        <span class="kort-tekst">{!! $post->body !!}</span>
+                        <p class="kort-tekst"><b>Oppdatert: </b>{{ date( 'j M Y - H:i', strtotime($post->updated_at)) }}</p>
+                    </div>
+                    <div class="under-kort">
+                        <h2>{{ $post->title }}</h2>
+                    </div>
+                </div>
+            @endfor
+        </div>
+    </div>
+
     <div class="container">
         <div class="row">
             @foreach($posts as $post)
